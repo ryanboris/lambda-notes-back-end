fe4b6e533461497093bc9e45efdfe7d3952bc4a4
Ryan Boris <ryan.boris@gmail.com>
2018-12-20 09:04:38 -0500

    Creates GraphQLAPI for Users and Notes

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 const { prisma } = require("./generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
 
-async function main() {}
+const resolvers = {
+  Query: {
+    note(root, args, context) {
+      return context.prisma.note({ id: args.noteId });
+    },
 
-main().catch(e => console.error(e));
+    notesByUser(root, args, context) {
+      return context.prisma
+        .user({
+          id: args.userId
+        })
+        .notes();
+    }
+  },
+  Mutation: {
+    createUser(root, args, context) {
+      return context.prisma.createUser({
+        name: args.name
+      });
+    },
+
+    createNote(root, args, context) {
+      return context.prisma.createNote({
+        title: args.title,
+        textbody: args.textbody,
+        author: {
+          connect: { id: args.userId }
+        }
+      });
+    }
+  },
+  User: {
+    notes(root, args, context) {
+      return context.prisma
+        .user({
+          id: root.id
+        })
+        .notes();
+    }
+  },
+
+  Note: {
+    author(root, args, context) {
+      return context.prisma
+        .note({
+          id: root.id
+        })
+        .author();
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./schema.graphql",
+  resolvers,
+  context: {
+    prisma
+  }
+});
+
+server.start(() =>
+  console.log("Server is up and running on http://localhost:4000")
+);
